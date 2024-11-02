4c4e465f75965b36cfe06efeb4eee140a836951c
ReetuInkila <inkilareetu@gmail.com>
2024-11-02 19:29:55 +0200

    app fetch times from sensor

--- react-native-app/Home.js
+++ react-native-app/Home.js
@@ -1,11 +1,63 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Button, Text, StyleSheet } from 'react-native';
+import { DataTable } from 'react-native-paper';
 
-export default function HomeScreen({ pairs, rounds }) {
+export default function HomeScreen() {
+  const [pairs, setPairs] = useState(null);
+  const [rounds, setRounds] = useState(null);
+  const [data, setData] = useState({});
+  const [loading, setLoading] = useState(false);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch('http://192.168.4.1/ski');
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setPairs("Error");
+      setRounds("Error");
+      setData([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <View>
-      <Text>Pairs: {pairs}</Text>
-      <Text>Rounds: {rounds}</Text>
+      {loading ? (
+        <Text>Loading...</Text>
+      ) : (
+        <>
+          <DataTable>
+            <DataTable.Header>
+              <DataTable.Title>Pari</DataTable.Title>
+              <DataTable.Title>Kierros</DataTable.Title>
+              <DataTable.Title>T1</DataTable.Title>
+              <DataTable.Title>T2</DataTable.Title>
+              <DataTable.Title>T Tot</DataTable.Title>
+            </DataTable.Header>
+
+
+            <DataTable.Row>
+              <DataTable.Cell>{1}</DataTable.Cell>
+              <DataTable.Cell>{1}</DataTable.Cell>
+              <DataTable.Cell>{data.t1}</DataTable.Cell>
+              <DataTable.Cell>{data.t2}</DataTable.Cell>
+              <DataTable.Cell>{data.t1 + data.t2}</DataTable.Cell>
+            </DataTable.Row>
+          </DataTable>
+        </>
+      )}
+      <Button title="Reload" onPress={fetchData} />
     </View>
   );
 }
