f28cded67a45f47da3929e7aa4263477b7835c60
Reetu Inkilä <inkilareetu@gmail.com>
2026-01-18 20:12:51 +0200

    Migrate project from React Native to Swift
    Removed the entire React Native (Expo) implementation under skiTesterApp, including all source code, assets, configuration, and dependencies. Added a new Swift-based Xcode project under swiftApp/SkiTesterApp, including initial Swift source files, assets, and project configuration. This commit represents a full migration of the app's codebase from React Native to native Swift.

---
+++ swiftApp/SkiTesterApp/SkiTesterApp/StartView.swift
@@ -0,0 +1,55 @@
+//
+//  StartView.swift
+//  SkiTesterApp
+//
+//  Created by Reetu Inkilä on 12.1.2026.
+//
+import SwiftUI
+import Combine
+
+struct StartView: View {
+    @Binding var isInfoVisible: Bool
+    @EnvironmentObject var store: AppStore
+
+    let oldResults = false
+
+    var body: some View {
+        VStack(spacing: 20) {
+
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity)
+                .aspectRatio(1, contentMode: .fit)
+
+            Button {
+                store.state.navigation = .settings
+            } label: {
+                Text("Uusi mittaus")
+            }
+
+            if oldResults {
+                Button {
+                    store.state.loadOldResults = true
+                    store.state.navigation = .settings
+                } label: {
+                    Text("Jatka edellistä")
+                }
+            }
+            Spacer()
+        }
+        .toolbar {
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button {
+                            isInfoVisible = true
+                        } label: {
+                            Image(systemName: "info.circle")
+                                .font(.system(size: 22))
+                        }
+                    }
+                }
+        .padding()
+        .background(Color.white)
+    }
+}
+
